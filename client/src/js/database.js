4e2f7325d8b53617f88c8362e2d29d9bc473df2a
Zach Beiser <zbeiser@gmail.com>
2023-03-07 11:20:51 -0800

    added method to update content to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,14 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
+  // Create a connection to the database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .put method on the store and pass in content.
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
 export const getDb = async () => {
   console.log('GET from the database');
 
-  // Create a connection to the database database and version we want to use.
+  // Create a connection to the database and version we want to use.
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
